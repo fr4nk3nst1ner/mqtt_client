17b241edece43c517dad296356dc3a64061e8c7f
fr4nk3nst1ner <jonathan.stines@gmail.com>
2019-02-20 22:06:05 -0600

    Update mqtt_client.py

--- mqtt_client.py
+++ mqtt_client.py
@@ -5,8 +5,10 @@ broker="<exact_broker_FQDN>"
 port=8883
 
 def on_connect(client, userdata, flags, rc):
-  client.subscribe('#', qos = 1)        # Subscribe to all topics
-  client.subscribe('$SYS/#')            # Broker Status (Mosquitto)
+  # Subscribe to all topics
+  client.subscribe('#', qos = 1)     
+  # Broker Status (Mosquitto)
+  client.subscribe('$SYS/#')            
 
 def on_log(client, userdata, level, buf):
   print("buffer ",buf)
@@ -17,12 +19,15 @@ def on_disconnect(client, userdata, rc):
 def on_message(client, userdata, msg):
   print(msg.topic+" "+str(msg.payload))
 
-client1=paho.Client("control1") #create client object
+#create client object
+client1=paho.Client("control1") 
 client1.on_log=on_log
 client1.tls_set(ca_certs='rootCA.pem',certfile='certificate.crt',keyfile='private.pem')
 
 client1.on_connect = on_connect
 client1.on_disconnect = on_disconnect
 client1.on_message = on_message
-client1.connect(broker,port) #establish connection
+
+#establish connection
+client1.connect(broker,port) 
 client1.loop_forever()
